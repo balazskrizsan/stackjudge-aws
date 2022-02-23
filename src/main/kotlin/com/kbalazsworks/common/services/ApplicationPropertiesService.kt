f27306fd291e07ef626732b7ad656a77b28efbf1
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-02-23 20:22:03 +0100

    Application props from env

--- src/main/kotlin/com/kbalazsworks/common/services/ApplicationPropertiesService.kt
+++ src/main/kotlin/com/kbalazsworks/common/services/ApplicationPropertiesService.kt
@@ -1,10 +1,20 @@
 package com.kbalazsworks.common.services
 
+import org.eclipse.microprofile.config.inject.ConfigProperty
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class ApplicationPropertiesService {
-    fun getAwsS3CdnBucket() = ""
-    fun getAwsAccessKey() = ""
-    fun getAwsSecretKey() = ""
+
+    @ConfigProperty(name = "aws.access_key")
+    var awsAccessKeyVal: String = ""
+    fun getAwsAccessKey() = awsAccessKeyVal
+
+    @ConfigProperty(name = "aws.secret_key")
+    var awsSecretKeyVal: String = ""
+    fun getAwsSecretKey() = awsSecretKeyVal
+
+    @ConfigProperty(name = "aws.s3.cdn_bucket")
+    var awsS3CdnBucketVal: String = ""
+    fun getAwsS3CdnBucket() = awsS3CdnBucketVal
 }
