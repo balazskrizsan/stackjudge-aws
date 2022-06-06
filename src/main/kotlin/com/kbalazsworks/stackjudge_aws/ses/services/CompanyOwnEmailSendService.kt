fd1e7d25a712c6e5d0407bf5145ed60ac1b1af3f
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-06-06 10:39:46 +0200

    CS

--- src/main/kotlin/com/kbalazsworks/stackjudge_aws/ses/services/CompanyOwnEmailSendService.kt
+++ src/main/kotlin/com/kbalazsworks/stackjudge_aws/ses/services/CompanyOwnEmailSendService.kt
@@ -8,13 +8,15 @@ import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class CompanyOwnEmailSendService(private val sendService: SendService, private val templateService: TemplateService) {
-    private val subject = "StackJudge - Company Own Request"
+    companion object {
+        private val SUBJECT = "StackJudge - Company Own Request"
+    }
 
     @Throws(SesSendException::class)
     fun send(companyOwnRawEmail: CompanyOwnRawEmail) {
         val companyOwnHtml = templateService.render("mail/company_own.html", companyOwnRawEmail.contex)
         val companyOwnText = templateService.render("mail/company_own.txt", companyOwnRawEmail.contex)
 
-        sendService.send(CompanyOwnEmail(companyOwnRawEmail.to, subject, companyOwnHtml, companyOwnText))
+        sendService.send(CompanyOwnEmail(companyOwnRawEmail.to, SUBJECT, companyOwnHtml, companyOwnText))
     }
 }
