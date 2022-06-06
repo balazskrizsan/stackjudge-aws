5e93391b6f96ea62584eb8d781674628a66f94da
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-06-06 10:39:17 +0200

    CS

--- src/main/kotlin/com/kbalazsworks/stackjudge_aws/s3/value_objects/Put.kt
+++ src/main/kotlin/com/kbalazsworks/stackjudge_aws/s3/value_objects/Put.kt
@@ -1,7 +1,6 @@
 package com.kbalazsworks.stackjudge_aws.s3.value_objects
 
 import com.kbalazsworks.stackjudge_aws.s3.enums.CdnNamespaceEnum
-import org.jboss.resteasy.reactive.multipart.FileUpload
 
 data class Put(
     var cdnNamespaceEnum: CdnNamespaceEnum,
